9995a559674400df02a935145d6e0ed2efc8f1f2
dreamjade <chc860827@gmail.com>
2023-07-14 05:07:27 -0700

    code v0.9.1

--- toycoronagraph/psf.py
+++ toycoronagraph/psf.py
@@ -7,23 +7,24 @@ def Wavefront_pos(x,y,pupil_grid):
     aperture = evaluate_supersampled(make_circular_aperture(1), pupil_grid, 4)
     return aperture * np.exp(2j * np.pi * (pupil_grid.x * x + pupil_grid.y * y))
 
-def psf_chunk(i, img_pixel, psf_range):
+def psf_chunk(i_psf_range, img_pixel, pupil_grid, prop, lyot_stop, coro):
+    x = 2*i_psf_range / img_pixel
+    wf = Wavefront(Wavefront_pos(x, 0,pupil_grid))
+    img = prop(lyot_stop(coro(wf))).intensity
+    return i, img.to_dict()["values"].reshape(img_pixel, img_pixel)
+
+def psf_calculation(charge, img_pixel=512, psf_range=16, num_cores = 4):
     pupil_grid = make_pupil_grid(1024, 1.5)
     focal_grid = make_focal_grid(16, 16)
     prop = FraunhoferPropagator(pupil_grid, focal_grid)
     lyot_mask = evaluate_supersampled(make_circular_aperture(0.5), pupil_grid, 4)
     coro = VortexCoronagraph(pupil_grid, charge=10)
     lyot_stop = Apodizer(lyot_mask)
-    x = 2*i*psf_range / img_pixel
-    wf = Wavefront(Wavefront_pos(x, 0,pupil_grid))
-    img = prop(lyot_stop(coro(wf))).intensity
-    return i, img.to_dict()["values"].reshape(img_pixel, img_pixel)
-
-def psf_calculation(charge, img_pixel=512, psf_range=16, num_cores = 4):
+    
     chunk_size = img_pixel // (2*num_cores)
     psfs = np.empty((img_pixel//2+1, img_pixel, img_pixel))
     pool = mp.Pool(processes=num_cores)
-    results = [pool.apply_async(psf_chunk, args=(i, img_pixel, psf_range)) for i in range(img_pixel//2+1)]
+    results = [pool.apply_async(psf_chunk, args=(i*psf_range, img_pixel, pupil_grid, prop, lyot_stop, coro)) for i in range(img_pixel//2+1)]
     pool.close()
     pool.join()
     for result in results:
@@ -32,7 +33,7 @@ def psf_calculation(charge, img_pixel=512, psf_range=16, num_cores = 4):
     np.save('psfs_c'+str(charge)+'.npy', psfs)
     return psfs
 
-def cir_psf(pre_img, img_pixel, psf_range, rot_number, psfs_name=None):
+def cir_psf(pre_img, img_pixel=512, psf_range=16, rot_number=360, psfs_name=None):
     chunk_img = np.zeros([img_pixel, img_pixel])
     if psfs_name==None:
         for i in range(img_pixel//2+1):
