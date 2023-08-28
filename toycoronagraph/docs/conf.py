7c5b0e5620432680babc5c337f4c364e6ba9e9cf
dreamjade <chc860827@gmail.com>
2023-08-28 16:47:33 -0700

    docs

--- toycoronagraph/docs/conf.py
+++ toycoronagraph/docs/conf.py
@@ -20,7 +20,8 @@ release = '1.5.2'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx.ext.autodoc', 
-              'sphinx.ext.napoleon']
+              'sphinx.ext.napoleon',
+              'sphinx.ext.githubpages']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
